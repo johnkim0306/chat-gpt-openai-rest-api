3808385acb2b4194a0343bf5456d477a9fee1334
John Kim <mkcokr2176@gmail.com>
2024-03-10 23:10:46 -0700

    Implementation of the basic code

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ChatGptAiModule } from './chat-gpt-ai/chat-gpt-ai.module';
+import { ConfigModule } from '@nestjs/config';
+import { ChatCompletionApiModule } from './chat-completion-api/chat-completion-api.module';
+
+@Module({
+  imports: [ChatGptAiModule, ConfigModule.forRoot(), ChatCompletionApiModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
